a9b92e19d4f5f7f9149dc0c9b07ef7bd7758584c
Matti Ryhänen <mryhanen@gmail.com>
2011-09-20 23:42:51 +0200

    fetch icon image

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/', function(req, res){
 
 app.get('/pos', function(req, res) {
 	//var url = "http://xn--ssongsmat-v2a.nu/w/api.php?action=ask&q=[[Fruktträd::%2B]]&po=Artikel|Bild|Ikontyp|Beskrivning|Koordinater&format=json";
-	var url = "http://xn--ssongsmat-v2a.nu/w/api.php?action=ask&q=%5B%5BFrukttr%C3%A4d%3A%2B%5D%5D&po=Artikel|Bild|Ikontyp|Beskrivning|Koordinater&format=json";
+	var url = "http://xn--ssongsmat-v2a.nu/w/api.php?action=ask&q=%5B%5BFrukttr%C3%A4d%3A%2B%5D%5D&po=Artikel|Bild|Ikon|Ikontyp|Beskrivning|Koordinater&format=json";
 	var client = new httpclient.httpclient();	
 
 	client.perform(url, "GET", function(result) {
