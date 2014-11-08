863832b9acb90a6d177637f0b9a6d45392b3df16
rotsee <leo_wallentin@hotmail.com>
2014-11-08 13:44:19 +0100

    Upgrade node.js

--- server.js
+++ server.js
@@ -3,42 +3,35 @@
  */
 
 var express = require('express');
+//require('express-namespace');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var errorHandler = require('errorhandler');
 
-require('express-namespace');
+var app = express();
 
-var app = module.exports = express.createServer();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('port', 3000);
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.set('port', 3000);
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(cookieParser());
 
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
+app.use(express.static(__dirname + '/public'));
 
-  // Use logging middleware
-  //app.use(express.logger());
-  //app.use(logging.requestLogger);
-  
-  //app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  
+if ('development' == app.get('env')) {
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }));
   app.set('port', 3001);
-});
+}
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
+if ('production' == app.get('env')) {
+  app.use(errorHandler());
+}
 
 require('./apps/fruktkartan/routes')(app);
 
-app.listen(app.settings.port, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var server = app.listen(app.settings.port, function(){
+  console.log("Express server listening on port %d in %s mode", app.listen().address().port, app.settings.env);
 });
